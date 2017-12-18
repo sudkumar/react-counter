a5a688437b8315562a0b773e2165710d150230a7
Sudhir Mitharwal <luckysud4@gmail.com>
2017-12-18 09:25:59 +0530

    (Update) resource js updated in webpack

--- webpack.config.docs.js
+++ webpack.config.docs.js
@@ -39,7 +39,7 @@ module.exports = {
   },
 
   output: {
-    publicPath: "/",
+    publicPath: "./",
     path: path.join(__dirname, "./docs"),
     filename: `js/[name].[hash].js`,
     chunkFilename: "js/[chunkhash].js"
